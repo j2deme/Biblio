0dba3d63c1ce954104c3c9fc7a1aa45f8772a8a4
j2deme <j2deme@gmail.com>
2016-05-09 08:42:03 -0500

    Añade binding de widgets con Butterknife

--- app/src/main/java/com/example/j2deme/biblio/MainActivity.java
+++ app/src/main/java/com/example/j2deme/biblio/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.j2deme.biblio;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.txtTitulo)
+    TextInputEditText txtTitulo;
+    @BindView(R.id.txtPaginas)
+    TextInputEditText txtPaginas;
+    @BindView(R.id.txtAutor)
+    TextInputEditText txtAutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
